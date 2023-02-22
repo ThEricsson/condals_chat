9fdcd140df6c9b626be597816a0b8e35abf8ec41
ThEricsson <soc.l.eric@gmail.com>
2023-02-22 10:16:27 +0000

    Subscriber controller and publisher controller working

--- controllers/chat.js
+++ controllers/chat.js
@@ -3,10 +3,66 @@
 var mqttConn = require("../connection/mqtt")
 
 var controller = {
+
     helloWorld: function (req, res) {
         return res.status(200).send({
             message: "Hello World"
         })
+    },
+    
+    //Metodo para suscribirse a un chat
+    subscribe: function (req, res) {
+        
+        let topic = req.query.topic
+        var message = "null"
+        
+        mqttConn.subscribe([topic], (error) => {
+            if (!error){
+                console.log(`Subscribe to topic '${topic}'`)
+            }
+
+            mqttConn.on('message', (topic, payload) => {
+                message = payload.toString()
+            
+                console.log({
+                    "topic": topic,
+                    "message": message,
+                    ressponse: "subscibed"
+                    
+                })
+
+                return res.status(200).send({
+                    "topic": topic,
+                    "message": message,
+                    ressponse: "subscibed"
+                    
+                }) 
+            })
+        })              
+
+    },
+
+    //Metodo para publicar en un chat
+    publish: function(req, res){
+        let topic = req.query.topic
+        let message = req.query.message
+
+        mqttConn.publish(topic, message, { qos: 0, retain: true }, (error) => {
+            if (error) {
+              console.error(error)
+              return res.status(500).send({
+                response: error
+              })
+            } else {
+                return res.status(200).send({
+                    response: "sended"
+                })
+            }
+
+        })
+
+        
+
     }
 
 }
--- routes/chat.js
+++ routes/chat.js
@@ -6,5 +6,7 @@ var ChatController = require("../controllers/chat")
 var router = express.Router()
 
 router.get("/helloworld", ChatController.helloWorld)
+router.get("/publish", ChatController.publish)
+router.get("/subscribe", ChatController.subscribe)
 
 module.exports = router;
