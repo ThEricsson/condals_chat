f2a89244452e5a3c7ac40fc412436c599ac68e1d
ThEricsson <soc.l.eric@gmail.com>
2023-02-23 11:37:45 +0000

    Backend woriking

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-"use strict"
+"use strict";
 
-var express = require('express')
-var app = express()
+var express = require("express");
+const cors = require("cors");
+
+var app = express();
 
 //Cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Acces-Control-Allow-Credentials', true)
-    next();
-});
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Acces-Control-Allow-Credentials", true);
+//   next();
+// });
+
+app.use(cors());
 
 //Rutes
-var chat_routes = require("./routes/chat")
+var chat_routes = require("./routes/chat");
 
-app.use("/", chat_routes)
+app.use("/", chat_routes);
 
 module.exports = app;
--- controllers/chat.js
+++ controllers/chat.js
@@ -1,70 +1,61 @@
-"use strict"
+"use strict";
 
-var mqttConn = require("../connection/mqtt")
+var mqttConn = require("../connection/mqtt");
 
 var controller = {
+  helloWorld: function (req, res) {
+    return res.status(200).send({
+      message: "Hello World",
+    });
+  },
 
-    helloWorld: function (req, res) {
+  //Metodo para suscribirse a un chat
+  subscribe: function (req, res) {
+    let topic = req.query.topic;
+    var message = "null";
+
+    mqttConn.subscribe([topic], (error) => {
+      if (!error) {
+        console.log(`Subscribe to topic '${topic}'`);
+      }
+
+      mqttConn.on("message", (topic, payload) => {
+        message = payload.toString();
+
+        console.log({
+          topic: topic,
+          message: message,
+          ressponse: "subscibed",
+        });
+
         return res.status(200).send({
-            message: "Hello World"
-        })
-    },
-    
-    //Metodo para suscribirse a un chat
-    subscribe: function (req, res) {
-        
-        let topic = req.query.topic
-        var message = "null"
-        
-        mqttConn.subscribe([topic], (error) => {
-            if (!error){
-                console.log(`Subscribe to topic '${topic}'`)
-            }
+          topic: topic,
+          message: message,
+          ressponse: "subscibed",
+        });
+      });
+    });
+  },
 
-            mqttConn.on('message', (topic, payload) => {
-                message = payload.toString()
-            
-                console.log({
-                    "topic": topic,
-                    "message": message,
-                    ressponse: "subscibed"
-                    
-                })
+  //Metodo para publicar en un chat
+  publish: function (req, res) {
+    console.log(req.query);
+    let topic = req.query.topic;
+    let message = req.query.message;
 
-                return res.status(200).send({
-                    "topic": topic,
-                    "message": message,
-                    ressponse: "subscibed"
-                    
-                }) 
-            })
-        })              
+    mqttConn.publish(topic, message, { qos: 0, retain: true }, (error) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send({
+          response: error,
+        });
+      } else {
+        return res.status(200).send({
+          response: "sended",
+        });
+      }
+    });
+  },
+};
 
-    },
-
-    //Metodo para publicar en un chat
-    publish: function(req, res){
-        let topic = req.query.topic
-        let message = req.query.message
-
-        mqttConn.publish(topic, message, { qos: 0, retain: true }, (error) => {
-            if (error) {
-              console.error(error)
-              return res.status(500).send({
-                response: error
-              })
-            } else {
-                return res.status(200).send({
-                    response: "sended"
-                })
-            }
-
-        })
-
-        
-
-    }
-
-}
-
-module.exports = controller
+module.exports = controller;
